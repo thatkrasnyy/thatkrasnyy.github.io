66a366026f614818bca8146206df42b67f5cc829
thatoranzhevyy <51875563+thatoranzhevyy@users.noreply.github.com>
2020-12-21 14:07:14 +0600

    deploy v1

---
+++ precache-manifest.0d23a582f7fda5b11113deadfc7c8a65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05a96c3834052244c073",
+    "url": "/css/chunk-vendors.1151dbc4.css"
+  },
+  {
+    "revision": "5d1da7e216c3cfa5886082784263b316",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d853e77cf07767305001",
+    "url": "/js/app.30cb5375.js"
+  },
+  {
+    "revision": "05a96c3834052244c073",
+    "url": "/js/chunk-vendors.6565a338.js"
+  },
+  {
+    "revision": "70f4411622ce7d7ddcebe9fdce799b1c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
